2b4f4ed7ef0e00a718e30b5ddee81720d2377152
desrikomati999 <devisrikomati1221@gmail.com>
2020-01-21 15:37:45 +0000

    flexbox

---
+++ start.py
@@ -0,0 +1,10 @@
+str=input("str:")
+nmbr=input("nmbr:")
+for number in range(int(nmbr)):
+    operation=input("operation:")
+    if operation.lower==find:
+        a=input("a:")
+        print(str.find(a))
+    elif: if operation.lower==count:
+        b=input("b:")
+        print(str.count(b))
